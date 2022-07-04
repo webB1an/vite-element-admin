67c8a4a1f9e30deab5a5a74a57f17cb0abd527d5
webB1an <webB1anyaoyao@gmail.com>
2022-07-04 17:11:20 +0800

    feat: split chunk

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
+import path, { resolve } from 'path'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+// import DefineOptions from 'unplugin-vue-define-options/vite'
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   const env = loadEnv(mode, resolve(__dirname, 'env'))
-  console.log(loadEnv(mode, resolve(__dirname, 'env')), env.VITE_APP_BASE_API, '-')
+
+  const baseApiProxy = env.VITE_APP_BASE_API ? {
+    [env.VITE_APP_BASE_API]: {
+      target: 'https://admin.uat.dusto-yc.com',
+      changeOrigin: true,
+      secure: false,
+      rewrite: (path) => path.replace(`^${env.VITE_APP_BASE_API}`, '')
+    }
+  } : {}
+
   return defineConfig({
     base: './',
     envDir: resolve(__dirname, 'env'),
@@ -14,20 +25,44 @@ export default ({ mode }) => {
       cors: true, // 允许跨域
       // 设置代理，根据我们项目实际情况配置
       proxy: {
-        [env.VITE_APP_BASE_API]: {
-          target: 'http://xxx.xxx.xxx.xxx:8000',
-          changeOrigin: true,
-          secure: false,
-          rewrite: (path) => path.replace(`^${env.VITE_APP_BASE_API}`, '')
-        }
+        ...baseApiProxy
       }
     },
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      // DefineOptions(),
+      createSvgIconsPlugin({
+        iconDirs: [path.resolve(process.cwd(), 'src/icons')],
+        symbolId: 'icon-[dir]-[name]'
+      })
+    ],
     resolve: {
       alias: {
         '@': resolve(__dirname, 'src')
       },
       extensions: ['.js', '.ts', '.jsx', '.tsx', '.json']
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: (id) => {
+            if (id.includes('element-plus')) {
+              return 'element-plus'
+            }
+            if (id.includes('vue-router')) {
+              return 'vue-router'
+            }
+            if (id.includes('vuex')) {
+              return 'vuex'
+            }
+            if (id.includes('lodash-es')) {
+              return 'lodash-es'
+            }
+
+            return 'vendor'
+          }
+        }
+      }
     }
   })
 }

9082ae37ce9d64696dcfbcbc822bb9be7c7ec2ce
webB1an <webB1anyaoyao@gmail.com>
2022-07-04 17:08:51 +0800

    feat: add svg icon

---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,39 @@
+<template>
+  <svg :class="svgClass" aria-hidden="true">
+    <use :href="symbolId" />
+  </svg>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  prefix: {
+    type: String,
+    default: 'icon'
+  },
+  svgClass: {
+    type: String,
+    default: ''
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  color: {
+    type: String,
+    default: 'red'
+  }
+})
+
+const symbolId = computed(() => `#${props.prefix}-${props.name}`)
+</script>
+
+<style scoped>
+svg {
+  width: 1em;
+  height: 1em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>

f254157c48f3ca2f0270568f49745e08e9d41ccb
webB1an <webB1an@users.noreply.github.com>
2022-07-12 08:35:33 +0000

    Updating f85fa5271d018fe61184bd19c6329a048d656b19

--- assets/detail.54e432ed.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.1678ed6d.js";import"./vendor.916f5b13.js";const r={};function t(d,n){return" detail "}var c=e(r,[["render",t]]);export{c as default};
---
+++ assets/detail.88afc63c.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.e8412413.js";import"./vendor.7b8a45ac.js";const r={};function t(a,c){return" detail "}var o=e(r,[["render",t]]);export{o as default};

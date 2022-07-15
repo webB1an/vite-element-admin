7fe0b0129957e06acf32d1cb585368b35b996e68
webB1an <webB1anyaoyao@gmail.com>
2022-07-15 17:43:30 +0800

    feat: add rich-text

---
+++ src/components/Tinymces/components/Editor.ts
@@ -0,0 +1,138 @@
+/**
+ * Copyright (c) 2018-present, Ephox, Inc.
+ *
+ * This source code is licensed under the Apache 2 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import { ScriptLoader } from '../ScriptLoader'
+import { getTinymce } from '../TinyMCE'
+import { isTextarea, mergePlugins, uuid, isNullOrUndefined, initEditor } from '../Utils'
+import { editorProps, IPropTypes } from './EditorPropTypes'
+import { h, defineComponent, onMounted, ref, Ref, toRefs, nextTick, watch, onBeforeUnmount, onActivated, onDeactivated } from 'vue'
+import { Editor as TinyMCEEditor, EditorEvent, TinyMCE } from 'tinymce'
+
+type EditorOptions = Parameters<TinyMCE['init']>[0];
+
+const renderInline = (ce: any, id: string, elementRef: Ref<Element | null>, tagName?: string) => ce(tagName || 'div', {
+  id,
+  ref: elementRef
+})
+
+const renderIframe = (ce: any, id: string, elementRef: Ref<Element | null>) => ce('textarea', {
+  id,
+  visibility: 'hidden',
+  ref: elementRef
+})
+
+export const Editor = defineComponent({
+  props: editorProps,
+  setup: (props: IPropTypes, ctx) => {
+    let conf = props.init ? { ...props.init } : {}
+    const { disabled, modelValue, tagName } = toRefs(props)
+    const element: Ref<Element | null> = ref(null)
+    let vueEditor: any = null
+    const elementId: string = props.id || uuid('tiny-vue')
+    const inlineEditor: boolean = (props.init && props.init.inline) || props.inline
+    const modelBind = !!ctx.attrs['onUpdate:modelValue']
+    let mounting = true
+    const initialValue: string = props.initialValue ? props.initialValue : ''
+    let cache = ''
+
+    const getContent = (isMounting: boolean): () => string => (modelBind
+      ? () => (modelValue?.value ? modelValue.value : '')
+      : () => (isMounting ? initialValue : cache))
+
+    const initWrapper = (): void => {
+      const content = getContent(mounting)
+      const finalInit = {
+        ...conf,
+        readonly: props.disabled,
+        selector: `#${elementId}`,
+        language: props.language || 'zh_CN',
+        plugins: mergePlugins(conf.plugins, props.plugins),
+        toolbar: props.toolbar || (conf.toolbar),
+        inline: inlineEditor,
+        setup: (editor: TinyMCEEditor) => {
+          vueEditor = editor
+          editor.on('init', (e: EditorEvent<any>) => initEditor(e, props, ctx, editor, modelValue, content))
+          if (typeof conf.setup === 'function') {
+            conf.setup(editor)
+          }
+        }
+      }
+      if (isTextarea(element.value)) {
+        element.value.style.visibility = ''
+      }
+      getTinymce().init(finalInit)
+      mounting = false
+    }
+    watch(disabled, (disable) => {
+      if (vueEditor !== null) {
+        if (typeof vueEditor.mode?.set === 'function') {
+          vueEditor.mode.set(disable ? 'readonly' : 'design')
+        } else {
+          vueEditor.setMode(disable ? 'readonly' : 'design')
+        }
+      }
+    })
+    watch(tagName, () => {
+      if (!modelBind) {
+        cache = vueEditor.getContent()
+      }
+      getTinymce()?.remove(vueEditor)
+      nextTick(() => initWrapper())
+    })
+    onMounted(() => {
+      if (getTinymce() !== null) {
+        initWrapper()
+      } else if (element.value && element.value.ownerDocument) {
+        const channel = props.cloudChannel ? props.cloudChannel : '6'
+        const apiKey = props.apiKey ? props.apiKey : 'no-api-key'
+        const scriptSrc: string = isNullOrUndefined(props.tinymceScriptSrc)
+          ? `https://cdn.tiny.cloud/1/${apiKey}/tinymce/${channel}/tinymce.min.js`
+          : props.tinymceScriptSrc
+        ScriptLoader.load(
+          element.value.ownerDocument,
+          scriptSrc,
+          initWrapper
+        )
+      }
+    })
+    onBeforeUnmount(() => {
+      if (getTinymce() !== null) {
+        console.log('vueEditor', vueEditor)
+        console.log('getTinymce()', getTinymce())
+        nextTick(() => {
+          getTinymce().remove(vueEditor)
+        })
+      }
+    })
+    if (!inlineEditor) {
+      onActivated(() => {
+        if (!mounting) {
+          initWrapper()
+        }
+      })
+      onDeactivated(() => {
+        if (!modelBind) {
+          cache = vueEditor.getContent()
+        }
+        getTinymce()?.remove(vueEditor)
+      })
+    }
+    const rerender = (init: EditorOptions) => {
+      cache = vueEditor.getContent()
+      getTinymce()?.remove(vueEditor)
+      conf = { ...conf, ...init }
+      nextTick(() => initWrapper())
+    }
+    ctx.expose({
+      rerender
+    })
+    return () => (inlineEditor
+      ? renderInline(h, elementId, element, props.tagName)
+      : renderIframe(h, elementId, element))
+  }
+})
---
+++ src/views/components/tinymce.vue
@@ -0,0 +1,40 @@
+<template>
+  <editor
+    v-model="content"
+    :api-key="apiKey"
+    :plugins="['advlist anchor autolink autosave code codesample directionality emoticons fullscreen hr image insertdatetime link lists media nonbreaking noneditable pagebreak paste preview print save searchreplace tabfocus table template textpattern visualblocks visualchars wordcount']"
+    :toolbar="['searchreplace bold italic underline strikethrough alignleft aligncenter alignright outdent indent  blockquote undo redo removeformat subscript superscript code codesample', 'hr bullist numlist link image charmap preview anchor pagebreak insertdatetime media table emoticons forecolor backcolor fullscreen']"
+    language="zh_CN"
+  />
+  <!-- <Tinymce v-model="content" /> -->
+  <el-card class="rich-text mt20">
+    <div v-html="content"></div>
+  </el-card>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+// import Tinymce from '@/components/Tinymce/index.vue'
+import Editor from '@/components/Tinymces/index'
+
+const apiKey = 'h98ls0ygp0wt8cr09l7qjiygrjfd8bqitqawd4rfh1ngujni'
+const content = ref(`
+<h1 style="text-align: center;">WOW Welcome Tinymce Demo</h1>
+<ul>
+  <li>1</li>
+  <li>2</li>
+  <li>3</li>
+</ul>
+`)
+
+// watch(content, () => {
+//   console.log(content.value)
+// })
+</script>
+
+<style>
+.rich-text {
+  min-height: 400px;
+  padding: 10px;
+}
+</style>

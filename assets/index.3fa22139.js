32b9975066e93a4b3d46718de30a5b5f4e93c4a1
webB1an <webB1an@users.noreply.github.com>
2022-07-20 02:08:15 +0000

    Updating ecb9436a8cf009267f85614d10229ff7ba6cff7f

---
+++ assets/index.3fa22139.js
@@ -0,0 +1,1 @@
+import{b as s,r as a,o as r,c as i,w as p,h as c}from"./vendor.25a7e868.js";const l=c(" Github Repository "),d=s({setup(m){const o=t=>window.open(t);return(t,e)=>{const n=a("el-button");return r(),i(n,{type:"primary",size:"large",onClick:e[0]||(e[0]=_=>o("https://github.com/webB1an/vite-element-admin"))},{default:p(()=>[l]),_:1})}}});export{d as default};
---
+++ assets/index.f4e06d16.js
@@ -0,0 +1,1 @@
+import{r as e,o as r,c as a,w as n,k as c}from"./vendor.25a7e868.js";import{_ as s}from"./index.aead7362.js";const _={};function l(i,f){const o=e("router-view"),t=e("el-alert");return r(),a(t,{title:"menu2",type:"info",closable:!1},{default:n(()=>[c(o)]),_:1})}var p=s(_,[["render",l]]);export{p as default};

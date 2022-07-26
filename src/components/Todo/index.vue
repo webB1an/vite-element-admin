66b3747f45c133269627bc3c26db25c3ba493eb0
webB1an <webB1anyaoyao@gmail.com>
2022-07-26 14:51:47 +0800

    style: code format

--- src/components/Todo/index.vue
+++ src/components/Todo/index.vue
@@ -1,12 +1,8 @@
 <template>
-  <el-col :span="6">
+  <el-col :span="6" class="todo-container">
     <el-card>
       <template #header>
-        <el-input
-          v-model="todo"
-          placeholder="Todo List"
-          @keydown.enter="addTodo(todo)"
-        />
+        <el-input v-model="todo" placeholder="Todo List" @keydown.enter="addTodo(todo)" />
       </template>
       <ul class="todo-list">
         <el-scrollbar height="200px">
@@ -14,13 +10,10 @@
             v-for="(item, index) in list"
             :key="item.id"
             class="todo-list-item"
-            :class="{'checked': item.completed}"
+            :class="{ checked: item.completed }"
             @click="doTodo(item.id)"
           >
-            <div
-              :class="{'radio-checked': item.completed}"
-              class="todo-list-item-radio"
-            ></div>
+            <div :class="{ 'radio-checked': item.completed }" class="todo-list-item-radio"></div>
             <span class="todo-list-item-des">{{ item.description }}</span>
             <span class="todo-list-item-close" @click.stop="delTodo(index)">✕</span>
           </li>
@@ -28,10 +21,7 @@
       </ul>
       <div class="todo-bottom">
         {{ incomplete.length }} 条未完成
-        <el-check-tag
-          :checked="active === TodoType.all"
-          @click="seeDifferentTodo(TodoType.all)"
-        >
+        <el-check-tag :checked="active === TodoType.all" @click="seeDifferentTodo(TodoType.all)">
           所有
         </el-check-tag>
         <el-check-tag
@@ -62,103 +52,105 @@ const { active, todo, list, incomplete, addTodo, doTodo, delTodo, seeDifferentTo
 <style lang="scss">
 $todoH: 50px;
 
-.el-card {
-  &__header {
-    padding: 0 !important;
-  }
-
-  &__body {
-    padding-top: 0 !important;
-    padding-bottom: 0 !important;
-  }
-}
-
-.el-input {
-  height: $todoH;
-
-  &__wrapper {
-    border-radius: unset !important;
-    box-shadow: unset !important;
-
-    &:hover {
-      box-shadow: unset !important;
+.todo-container {
+  .el-card {
+    &__header {
+      padding: 0 !important;
     }
 
-    &.is-focus {
-      box-shadow: unset !important;
+    &__body {
+      padding-top: 0 !important;
+      padding-bottom: 0 !important;
     }
   }
-}
 
-.todo-list {
-  &-item {
-    position: relative;
-    display: flex;
-    width: 100%;
+  .el-input {
     height: $todoH;
-    align-items: center;
-    font-size: 16px;
-    font-weight: bold;
-    cursor: pointer;
-    border-bottom: 1px solid #e9e9e9;
 
-    // &:not(:last-child) {
+    &__wrapper {
+      border-radius: unset !important;
+      box-shadow: unset !important;
 
-    // }
+      &:hover {
+        box-shadow: unset !important;
+      }
 
-    &:hover {
-      .todo-list-item-close {
-        visibility: visible;
+      &.is-focus {
+        box-shadow: unset !important;
       }
     }
+  }
 
-    &.checked {
-      color: #e9e9e9;
+  .todo-list {
+    &-item {
+      position: relative;
+      display: flex;
+      width: 100%;
+      height: $todoH;
+      align-items: center;
+      font-size: 16px;
+      font-weight: bold;
+      cursor: pointer;
+      border-bottom: 1px solid #e9e9e9;
 
-      .todo-list-item-des {
-        text-decoration: line-through;
-      }
-    }
+      // &:not(:last-child) {
 
-    &-radio {
-      display: inline-block;
-      width: 30px;
-      height: 30px;
-      text-align: center;
-      line-height: 30px;
-      margin-right: 10px;
-      border-radius: 50%;
-      border: 1px solid #e9ebf0;
+      // }
 
-      &.radio-checked {
-        border: 1px solid #6dea6d;
-
-        &::after {
-          content: '✓';
-          color: #6dea6d;
-          font-size: 20px;
+      &:hover {
+        .todo-list-item-close {
+          visibility: visible;
         }
       }
-    }
 
-    &-close {
-      visibility: collapse;
-      position: absolute;
-      right: 20px;
-      top: 50%;
-      transform: translateY(-50%);
-      text-decoration: unset;
-      color: #f5a9a9;
+      &.checked {
+        color: #e9e9e9;
+
+        .todo-list-item-des {
+          text-decoration: line-through;
+        }
+      }
+
+      &-radio {
+        display: inline-block;
+        width: 30px;
+        height: 30px;
+        text-align: center;
+        line-height: 30px;
+        margin-right: 10px;
+        border-radius: 50%;
+        border: 1px solid #e9ebf0;
+
+        &.radio-checked {
+          border: 1px solid #6dea6d;
+
+          &::after {
+            content: '✓';
+            color: #6dea6d;
+            font-size: 20px;
+          }
+        }
+      }
+
+      &-close {
+        visibility: collapse;
+        position: absolute;
+        right: 20px;
+        top: 50%;
+        transform: translateY(-50%);
+        text-decoration: unset;
+        color: #f5a9a9;
+      }
     }
   }
-}
 
-.todo-bottom {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  height: 50px;
-  color: #c8c9cc;
-  font-size: 12px;
+  .todo-bottom {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 50px;
+    color: #c8c9cc;
+    font-size: 12px;
+  }
 }
 </style>
